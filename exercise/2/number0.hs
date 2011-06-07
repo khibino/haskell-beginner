57c61d31d5adecec91d9224716a806d17072319d
HIBINO Kei <hibi@utmc.or.jp>
2011-06-08 08:15:23 +0900

    number command implementation.

---
+++ exercise/2/number0.hs
@@ -0,0 +1,15 @@
+
+import Control.Monad.State (MonadState(..), State, execState)
+
+number :: [String] -> State (Int, [String]) (Int, [String])
+number =  rec
+  where rec (i:is) =
+          do (n, os) <- get
+             put (n + 1 :: Int, os ++ [show n ++ ": " ++ i])
+             rec is
+        rec []     = get
+  
+main :: IO ()
+main =  do lines' <- fmap lines getContents
+           putStrLn $ unlines $ snd $
+             execState (number lines') (1, [])
---
+++ exercise/2/number1.hs
@@ -0,0 +1,17 @@
+
+import System.IO (isEOF)
+import Data.IORef (newIORef, readIORef, modifyIORef)
+
+number :: IO ()
+number = newIORef 1 >>= rec
+  where rec counter = do eof <- isEOF
+                         case eof of
+                           False -> do l <- getLine
+                                       n <- readIORef counter
+                                       putStrLn $ show n ++ ": " ++ l
+                                       modifyIORef counter (+1)
+                                       rec counter
+                           True  -> return ()
+
+main :: IO ()
+main = number
